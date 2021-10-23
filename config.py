654e89584df7aba560f1a36f5d96ddc451dc529e
Qais Attarwala <qais.attarwala0@yahoo.com>
2021-10-23 18:21:10 -0500

    Update config.py

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/fyurr'
+DATABASE_URL = 'postgresql://postgres@localhost:5432/fyurr'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
